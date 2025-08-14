0f4575dff6442b3750f6ccb668fb0f31e391858b
ackresponse-ar <sagar.gurung@ackresponse.com>
2025-08-14 17:33:35 +0100

    Changes

--- components/heroSection.tsx
+++ components/heroSection.tsx
@@ -38,7 +38,7 @@ const LandingPage = () => {
             <section className="my-6">
               <div className="mb-6">
                 <h1 className="text-[24px] text-white md:text-[28px] lg:text-[32px] xl:text-[36px] 2xl:text-[40px] font-extrabold leading-[1.2] mb-4 drop-shadow-lg tracking-wide">
-                  🎉 Namaste, Welcome to Nepali Mela 2025, UK test test 🎉
+                  🎉 Namaste, Welcome to Nepali Mela 2025, UK test test 1232322wdfsdf 🎉
                 </h1>
                 <div className="w-20 h-1 animate-gradient-shift rounded-full mb-4"></div>
               </div>
