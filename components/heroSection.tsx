8fdaf9fdfb015cf7368b561f01255d834dc4eadf
ackresponse-ar <sagar.gurung@ackresponse.com>
2025-09-05 00:59:14 +0100

    New video comp hght

--- components/heroSection.tsx
+++ components/heroSection.tsx
@@ -31,7 +31,7 @@ const HeroSection = () => {
           muted
           playsInline
           className="absolute inset-0 w-full h-full object-cover object-center"
-          style={{ width: '100%', height: '100%' }}
+          style={{ width: '100%', height: '120%' }}
         />
 
         {/* Dark Overlay */}
